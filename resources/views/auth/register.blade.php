adf9fc6f76b8088469f782290e2e6ad7ce696e5c
Hadi157 <hadiindrawan157@gmail.com>
2021-12-09 21:25:41 +0700

    register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Register</title>
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body class="antialiased">
+    <div class="container">
+        <div class="row" style="">
+            <div class="card form" style="
+            width: 30%;
+            margin: 0 auto;
+            margin-top: 100px;
+            background: #FFFFFF;
+            border-radius: 15px;
+            box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.25);
+            ">
+                <form class="p-3" style="">
+                    <div class="text-center mb-3">
+                        <h5><strong>Register</strong></h5>
+                    </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="email" class="form-control" id="name" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="confirmation" class="form-label">Confirmation Password</label>
+                        <input type="password" class="form-control" id="confirmation">
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary px-5" style="background: linear-gradient(270.62deg, #1289FF -25.07%, #A0B304 103.72%);
+                    border-radius: 20px; border: none; margin: 0 auto;">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
