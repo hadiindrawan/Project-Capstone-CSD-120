81b03d91c8f4a57b55ce79ed33562a2db64134b2
Hadi157 <hadiindrawan157@gmail.com>
2021-12-24 15:15:46 +0700

    remove js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
-let map = L.map("map").setView([-6.917464, 107.619125], 13);
+// let map = L.map("map").setView([-6.917464, 107.619125], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+// L.marker([-6.917464, 107.619125]).addTo(map);
